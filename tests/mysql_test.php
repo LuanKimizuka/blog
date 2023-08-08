6210e0556e2546189b8d7fe3f3ef379dbb5dcfb6
luan <luantorres233@outlook.com>
2023-08-08 11:49:35 -0300

    commit

--- tests/mysql_test.php
+++ tests/mysql_test.php
@@ -4,7 +4,7 @@ require_once '../core/conexao_mysql.php';
 require_once '../core/sql.php';
 require_once '../core/mysql.php';
 
-insert_teste ('Pedro', 'p.arrieiro@ifsp.edu.br', '123456');
+insert_teste ('Samuel', 'g.samuel@aluno.ifsp.edu.br', 'samu1234');
 buscar_teste();
 update_teste(38, 'murilo', 'silva@gmail.com');
 buscar_teste();
